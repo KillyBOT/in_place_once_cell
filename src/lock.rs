61fcc234830f20e56062b6138d009e4b053bb7e1
KillyBOT <edwards.kyle.j@gmail.com>
2025-08-03 16:57:47 -0400

    Added `InPlaceOnceLock`
    Most of the code is the same as `InPlaceOnceCell`, so it was pretty
    easy.

--- src/lock.rs
+++ src/lock.rs
@@ -1,18 +1,217 @@
 use std::cell::UnsafeCell;
+use std::fmt;
+use std::panic::{RefUnwindSafe, UnwindSafe};
 use std::sync::Once;
 
+// TODO: Add more documentation
+
+// TODO: Once `#![feature(never_type)]` is stabilized, remove this
+enum Never {}
+
+/// A cell that can only be mutated once.
 pub struct InPlaceOnceLock<T> {
+    value: UnsafeCell<T>,
     once: Once,
-    inner: UnsafeCell<T>,
 }
 
 impl<T> InPlaceOnceLock<T> {
+    /// Creates a new cell that has not been mutated.
     #[inline]
     #[must_use]
-    pub const fn new(value: T) -> InPlaceOnceLock<T> {
-        InPlaceOnceLock {
+    pub const fn new(value: T) -> Self {
+        Self {
+            value: UnsafeCell::new(value),
             once: Once::new(),
-            inner: UnsafeCell::new(value),
         }
+    }
+
+    #[inline]
+    #[must_use]
+    fn is_mutated(&self) -> bool {
+        self.once.is_completed()
+    }
+
+    /// # Safety
+    ///
+    /// The cell must be mutated.
+    #[inline]
+    unsafe fn get_unchecked(&self) -> &T {
+        debug_assert!(self.is_mutated());
+        unsafe { &*self.value.get() }
+    }
+
+    /// # Safety
+    ///
+    /// The cell must be mutated.
+    #[inline]
+    unsafe fn get_mut_unchecked(&mut self) -> &mut T {
+        debug_assert!(self.is_mutated());
+        self.value.get_mut()
+    }
+
+    /// Gets the reference to the underlying value.
+    ///
+    /// Returns `None` if the cell is not mutated.
+    #[inline]
+    pub fn get(&self) -> Option<&T> {
+        if self.is_mutated() {
+            // SAFETY: `self.is_mutated() == true`
+            Some(unsafe { self.get_unchecked() })
+        } else {
+            None
+        }
+    }
+
+    /// Gets a mutable reference to the underlying value.
+    ///
+    /// Returns `None` if the cell is not mutated.
+    pub fn get_mut(&mut self) -> Option<&mut T> {
+        if self.is_mutated() {
+            Some(self.value.get_mut())
+        } else {
+            None
+        }
+    }
+
+    /// Gets the contents of the cell, mutating it with `f` if the cell was never mutated.
+    ///
+    /// # Panics
+    ///
+    /// If `f` panics, the panic is propagated to the caller, and the cell is never fully mutated.
+    ///
+    /// `f` is not guaranteed to be reentrant, so calling the function after a panic also may
+    /// result in a panic.
+    #[inline]
+    pub fn get_or_mutate<F>(&self, f: F) -> &T
+    where
+        F: FnOnce(&mut T),
+    {
+        match self.get_or_try_mutate(|val: &mut T| {
+            f(val);
+            Ok::<(), Never>(())
+        }) {
+            Ok(..) => {}
+        }
+
+        // SAFETY: the previous code is guaranteed to mutate the cell
+        unsafe { self.get_unchecked() }
+    }
+
+    #[inline]
+    pub fn get_mut_or_mutate<F>(&mut self, f: F) -> &mut T
+    where
+        F: FnOnce(&mut T),
+    {
+        match self.get_or_try_mutate(|val: &mut T| {
+            f(val);
+            Ok::<(), Never>(())
+        }) {
+            Ok(..) => {}
+        }
+
+        // SAFETY: the previous code is guaranteed to mutate the cell
+        unsafe { self.get_mut_unchecked() }
+    }
+
+    /// Gets the contents of the cell, mutating it with `f` if the cell was never mutated.
+    ///
+    /// Returns an error if `f` returns an error during mutation.
+    pub fn get_or_try_mutate<F, E>(&self, f: F) -> Result<&T, E>
+    where
+        F: FnOnce(&mut T) -> Result<(), E>,
+    {
+        if let Some(val) = self.get() {
+            return Ok(val);
+        }
+
+        self.try_mutate(f)?;
+        // SAFETY: `try_mutate`, if it does not fail, is guaranteed to make the cell mutated.
+        Ok(unsafe { self.get_unchecked() })
+    }
+
+    /// Gets the mutable contents of the cell, mutating it with `f` if the cell was never mutated.
+    ///
+    /// Returns an error if `f` returns an error during mutation.
+    pub fn get_mut_or_try_mutate<F, E>(&mut self, f: F) -> Result<&mut T, E>
+    where
+        F: FnOnce(&mut T) -> Result<(), E>,
+    {
+        // Stupid but we can't borrow `self`.
+        if self.is_mutated() {
+            // SAFETY: `is_mutated` ensures that `get_mut_unchecked` is safe.
+            return Ok(unsafe { self.get_mut_unchecked() });
+        }
+
+        self.try_mutate(f)?;
+        // SAFETY: `try_mutate`, if it does not fail, is guaranteed to make the cell mutated.
+        Ok(unsafe { self.get_mut_unchecked() })
+    }
+
+    fn try_mutate<F, E>(&self, f: F) -> Result<(), E>
+    where
+        F: FnOnce(&mut T) -> Result<(), E>,
+    {
+        let mut res: Result<(), E> = Ok(());
+        // TODO: Poison on failure of `f` in order to enforce no reentrancy
+        self.once.call_once(
+            #[cold]
+            || {
+                // SAFETY: `try_init` is only called in `get_*_or_try_mutate`, meaning `self.inner` will
+                // always be non-null and not mutated.
+                let inner_mut_ref = unsafe { &mut *self.value.get() };
+                res = f(inner_mut_ref);
+            },
+        );
+
+        res
+    }
+
+    /// Consumes the cell, returning the wrapped value. Note that this occurs even when the cell
+    /// was never mutated.
+    #[inline]
+    pub fn into_inner(self) -> T {
+        // TODO: Make this a `pub const fn`.
+        self.value.into_inner()
+    }
+}
+
+unsafe impl<T: Sync> Sync for InPlaceOnceLock<T> {}
+
+impl<T: RefUnwindSafe + UnwindSafe> RefUnwindSafe for InPlaceOnceLock<T> {}
+impl<T: UnwindSafe> UnwindSafe for InPlaceOnceLock<T> {}
+
+impl<T: Default> Default for InPlaceOnceLock<T> {
+    #[inline]
+    fn default() -> Self {
+        Self::new(T::default())
+    }
+}
+
+impl<T: fmt::Debug> fmt::Debug for InPlaceOnceLock<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut d = f.debug_tuple("InPlaceOnceLock");
+        match self.get() {
+            Some(v) => d.field(v),
+            None => d.field(&format_args!("<untouched>")),
+        };
+
+        d.finish()
+    }
+}
+
+impl<T: PartialEq> PartialEq for InPlaceOnceLock<T> {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.get() == other.get()
+    }
+}
+
+impl<T: Eq> Eq for InPlaceOnceLock<T> {}
+
+impl<T> From<T> for InPlaceOnceLock<T> {
+    /// Creates a new `InPlaceOnceLock<T>` containing `value`. This new cell is not yet mutated.
+    #[inline]
+    fn from(value: T) -> Self {
+        Self::new(value)
     }
 }
